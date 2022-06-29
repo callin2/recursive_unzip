ab23ac93e4c2ee5786a7451615db4d61883e31bf
callin <callin2@gmail.com>
2022-06-29 10:24:26 +0900

    repackaging

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow() {
     mainWindow.loadFile('index.html')
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 
     return mainWindow
 }
